e55e82e842a0ec4e8c1ad3fa1a652677524b4c83
Илья <joker104_97@mail.ru>
2026-02-15 04:47:12 +0300

    Fix import paths for Header and Footer in SitemapPage

--- src/pages/SitemapPage.tsx
+++ src/pages/SitemapPage.tsx
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
-import { Header } from "@/components/Header";
-import { Footer } from "@/components/Footer";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { categories } from "@/lib/data";
 
