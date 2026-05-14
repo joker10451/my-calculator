2e80b3fc2f8ac1b7ea5eaf8c576cd562d4ead739
Илья <joker104_97@mail.ru>
2026-05-14 19:32:25 +0300

    fix: вернул i18n import  компоненты зависят от инициализации

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import './i18n';
 import { initGA } from "./lib/analytics/googleAnalytics";
 import { initConversionTracking } from "./lib/analytics/conversionTracking";
 import { initWebVitals } from "./lib/analytics/webVitals";
